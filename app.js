e62de22aa55170d8e40b7a3c54375ad7f9ce354a
mrynp <110476086+mrynp@users.noreply.github.com>
2024-04-04 14:37:35 +0800

    Add parallax effect

---
+++ app.js
@@ -0,0 +1,22 @@
+const title = document.querySelector(".title");
+const dots1 = document.querySelector(".dots1");
+const dots2 = document.querySelector(".dots2");
+const wavy1 = document.querySelector(".wavy1");
+const table = document.querySelector(".table");
+const wavy2 = document.querySelector(".wavy2");
+const woman = document.querySelector(".woman");
+
+document.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  // console.log(value)
+  title.style.marginTop = value * 1.1 + "px";
+
+  dots1.style.marginLeft = -value + "px";
+  dots2.style.marginLeft = value + "px";
+
+  woman.style.marginBottom = -value + "px";
+  woman.style.marginLeft = -value + "px";
+
+  wavy1.style.marginBottom = -value * 1.1 + "px";
+  table.style.marginBottom = -value * 1.2 + "px";
+});
